9ba6bc2d05240af3c6eb734827b357df655f233d
Jason Bingham <jbingham223@gmail.com>
2025-09-17 08:26:08 -0300

    shifted to collections schema

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const allPosts = await getCollection("blog");
+
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: "https://first-blog-build.netlify.app",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: allPosts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/posts/${post.id}`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
